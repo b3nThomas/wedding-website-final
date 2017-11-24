b420c5a706de921e2897ac5fbde3326fd829f6c4
b3nThomas <b3nadthomas@gmail.com>
2017-11-24 07:45:28 +0000

    renamed function

--- src/fixtures/RSVP.ts
+++ src/fixtures/RSVP.ts
@@ -1,6 +1,6 @@
-const createNumberDropdown = (limit: number) => {
+const createNumberSelectOptions = (start: number, limit: number) => {
     let template = ``;
-    let index = 1;
+    let index = start;
     while (index <= limit) {
         template += `<option value="${ index }">${ index }</option>`;
         index++;
@@ -17,16 +17,19 @@ export const RSVP = `
             </div>
             <div class='col-xs-1'></div>
         </div>
-        <div class='row'>
-            <div class='col-xs-1'></div>
-            <div class='col-xs-10'>
-                Number of Guests:
-                <select id='rsvp-guests' type='number'>
-                    ${ createNumberDropdown(10) }
-                </select>
+        <div class='rsvp-form'>
+            <div class='row'>
+                <div class='col-xs-1'></div>
+                <div class='col-xs-10' >
+                    <span class='font-moon-light'>Number of Guests</span>
+                    <select id='rsvp-guests' type='number'>
+                        ${ createNumberSelectOptions(1, 10) }
+                    </select>
+                </div>
+                <div class='col-xs-1'></div>
             </div>
-            <div class='col-xs-1'></div>
         </div>
+
     </div>
 `;
 
