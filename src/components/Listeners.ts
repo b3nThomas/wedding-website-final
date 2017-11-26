846031e9544741bd233779902dcb28129aff113d
b3nThomas <b3nadthomas@gmail.com>
2017-11-26 01:49:34 +0000

    can add more guest name inputs

--- src/components/Listeners.ts
+++ src/components/Listeners.ts
@@ -1,5 +1,10 @@
 export const rsvp = () => {
     $('#rsvp-guests').change(() => {
-        alert(`New value: ${$('#rsvp-guests').val()}`);
+        const totalGuests = Number($('#rsvp-guests').val());
+        let template = '';
+        for (let i = 0; i < totalGuests; i++) {
+            template += `<input type='text' class='rsvp-name'></input><br>`;
+        }
+        $('#rsvp-names').html(template);
     });
 };
