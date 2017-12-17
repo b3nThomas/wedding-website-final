4e45fc4f6ba8a96ac4b48ef28605096b885c7b80
b3nThomas <b3nadthomas@gmail.com>
2017-12-17 19:57:25 +0000

    venue page styling

--- src/fixtures/Venue.ts
+++ src/fixtures/Venue.ts
@@ -7,15 +7,15 @@ export const Venue = `
         </div>
         </br>
         <div class='row'>
-            <div id='map-container' class='col-xs-12'>
+            <div class='map-container col-xs-12'>
                 <iframe class='venue-map box-shadow' src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d66477.05148935101!2d-1.9113974717633855!3d51.74276147230118!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x688c98a90ca25866!2sCripps+Barn!5e0!3m2!1sen!2suk!4v1487940894833' width='100%' height='330px' frameborder='0' style='border:0' allowfullscreen></iframe>
             </div>
         </div>
-        <div class='row'>
-            <div id='accom-container' class='col-12 center'>
-                <a id='accom-link' class='accom-default theme-background-color font-moon-light' title='Click here to view the local accomodation'
-                    href='https://www.crippsbarn.com/directory/' style='text-decoration: none; color: white'>ACCOMMODATION</a>
-            </div>
+        <div class='accom-container font-segoe fs-s col-12 center'>
+            <p>For more information</p>
+            <p>visit the Cripps Barn <a href='https://www.crippsbarn.com/'>website</a></p>
+            <p>or view local accommodation <a href='https://www.crippsbarn.com/directory/'>here</a></p>
         </div>
     </div>
+    </br>
 `;
